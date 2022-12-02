49972a86f724683f9ac49b606d057be74dd9e1cd
Bojan Vilic <bojan.vilic@gmail.com>
2022-12-01 21:05:02 -0500

    Disabled dynamic colors

--- app/src/main/java/com/bojanvilic/crvenazvezdainfo/theme/Theme.kt
+++ app/src/main/java/com/bojanvilic/crvenazvezdainfo/theme/Theme.kt
@@ -1,20 +1,18 @@
 package com.bojanvilic.crvenazvezdainfo.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+
     val colors = when {
-        dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
-        dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
         darkTheme -> darkThemeColors
         else -> lightThemeColors
     }
